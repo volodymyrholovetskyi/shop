1bb9dd94d1d4d2f64c0017f8130f4272f6391c82
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2023-08-13 22:27:14 +0200

    Create Product model

---
+++ backend/src/main/java/com/vholovetskyi/shop/product/model/Product.java
@@ -0,0 +1,26 @@
+package com.vholovetskyi.shop.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long categoryId;
+    private String description;
+    private String fullDescription;
+    private BigDecimal price;
+    private String image;
+    private String slug;
+
+}
