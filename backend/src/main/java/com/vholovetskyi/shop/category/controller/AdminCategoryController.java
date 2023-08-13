cdb80f814f398cd9fc384b30e871c248063dae27
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2023-08-13 23:13:59 +0200

    Create AdminCategoryController class

---
+++ backend/src/main/java/com/vholovetskyi/shop/category/controller/AdminCategoryController.java
@@ -0,0 +1,58 @@
+package com.vholovetskyi.shop.category.controller;
+
+import com.github.slugify.Slugify;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import v.holovetskyi.shop.admin.category.controller.dto.AdminCategoryDto;
+import v.holovetskyi.shop.admin.category.model.AdminCategory;
+import v.holovetskyi.shop.admin.category.service.AdminCategoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/categories")
+@RequiredArgsConstructor
+public class AdminCategoryController {
+
+    public static final Long EMPTY_ID = null;
+    private final AdminCategoryService adminCategoryService;
+
+    @GetMapping
+    public List<AdminCategory> getCategories(){
+        return adminCategoryService.getCategories();
+    }
+
+    @GetMapping("/{id}")
+    public AdminCategory getCategory(@PathVariable Long id){
+        return adminCategoryService.getCategory(id);
+    }
+
+    @PostMapping
+    public AdminCategory createCategory(@RequestBody AdminCategoryDto adminCategoryDto){
+        return adminCategoryService.createCategory(mapToAdminCategory(EMPTY_ID, adminCategoryDto));
+    }
+
+    @PutMapping("/{id}")
+    public AdminCategory updateCategory(@PathVariable Long id, @RequestBody AdminCategoryDto adminCategoryDto){
+        return adminCategoryService.updateCategory(mapToAdminCategory(id, adminCategoryDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id){
+        adminCategoryService.deleteCategory(id);
+    }
+
+    private String slugifyCategoryName(String slug) {
+        Slugify slugify = new Slugify();
+        return slugify.withCustomReplacement("_", "-").slugify(slug);
+    }
+
+    private AdminCategory mapToAdminCategory(Long id, AdminCategoryDto adminCategoryDto) {
+        return AdminCategory.builder()
+                .id(id)
+                .name(adminCategoryDto.getName())
+                .description(adminCategoryDto.getDescription())
+                .slug(slugifyCategoryName(adminCategoryDto.getSlug()))
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/vholovetskyi/shop/category/controller/dto/AdminCategoryDto.java
@@ -0,0 +1,17 @@
+package com.vholovetskyi.shop.category.controller.dto;
+
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class AdminCategoryDto {
+    @NotBlank
+    @Length(min = 4)
+    private String name;
+    private String description;
+    @NotBlank
+    @Length(min = 4)
+    private String slug;
+}
